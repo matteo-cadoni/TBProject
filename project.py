8c6a6fd6b5a69be96a0444c0b0813960a14426d1
lorenzocapob <117085421+lorenzocapob@users.noreply.github.com>
2023-01-16 20:09:15 +0100

    moved scripts in src folder, simplified project

--- project.py
+++ project.py
@@ -13,16 +13,7 @@ import napari
 #import cv2 as cv
 #import logging  #maybe useful for debugging
 
-from loader import Loader
-from preprocess import Preprocessing
-from thresholding import Thresholding
-from postprocessing import Postprocessing
-from cropping import Cropping
-from interactivelabelling import InteractiveLabeling
-from interactive_config import InteractiveConfig, change_yaml
-from inference_visualization import Inference
-
-from visualization import visualize_all_list_napari, add_bounding_boxes, is_blurry_laplacian
+from mixed_functions import *
 
 
 def arguments_parser():
@@ -32,182 +23,28 @@ def arguments_parser():
                         help='configure file for thresholding experiments')
     return parser
 
+
+
 def main():
     parser = arguments_parser()
     pars_arg = parser.parse_args()
     
-    # read config as dictionary
+    # read config from input .yaml
     with open(pars_arg.config, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    ####### BEGIN INTERACTIVE CONFIGURATION #######
+    # Interactive config
     if config['load']['interactive_config']:
-        while True:
-            i_c = InteractiveConfig()
-            configurations = i_c.run()
-            if i_c.error:
-                continue
-            else:
-                break
-        config = change_yaml(configurations, config)
-    ####### END INTERACTIVE CONFIGURATION #######
-
-    ####### BEGIN LOADING #######
+        config = interactive_config(config)
+        
+    # Load data
     load_config = config['load']
-    loader = Loader(load_config['czi_path'], load_config['tile'])
-    loader.load()
-    if load_config['tile'] == 'None':
-        smear = loader.data_array
-        print(f"Smear succesfully loaded, shape: {smear.shape}")
-        total_number_bacilli = 0
-        counter_of_blurry_images = 0
-    else:
-        img = loader.data_array
-        print(f"Tile succesfully loaded, shape: {img.shape}")
-    ######## END LOADING ########
+    img, loader = load(load_config)
 
     if load_config['tile'] == 'None':
-        for tiles in range(0, smear.shape[0]): 
-            img = smear[tiles, :, :]
-            print("Tile: ", tiles)
-            if load_config['blurry_deselection']:
-                if is_blurry_laplacian(img):
-                    print("It's blurry")
-                    # counter of the blurry images:
-                    counter_of_blurry_images += 1
-                    pass
-                else:
-                    ######## BEGIN PREPROCESSING ########
-                    preprocess_config = config['preprocessing']
-                    preprocess = Preprocessing(img)
-                    if preprocess_config['algorithm'] == "sharp":
-                        preprocessed_img = preprocess.sharpen()
-                    if preprocess_config['algorithm'] == "rescale":
-                        preprocessed_img = preprocess.rescale()
-                    ######## END PREPROCESSING ########
-
-                    ######## BEGIN THRESHOLDING ########
-                    threshold_config = config['thresholding']
-                    threshold = Thresholding(preprocessed_img, threshold_config)
-                    thresholded_img = threshold.apply()
-                    ######## END THRESHOLDING ########
-
-                    ######## BEGIN POSTPROCESSING ########
-                    postprocessing_config = config['postprocessing']
-                    postprocess = Postprocessing(thresholded_img, postprocessing_config)
-                    whole_img_not_cleaned, final_image, num_bacilli, stats = postprocess.apply()
-                    total_number_bacilli += num_bacilli
-                    ######## END POSTPROCESSING ########
-            else:
-                ######## BEGIN PREPROCESSING ########
-                preprocess_config = config['preprocessing']
-                preprocess = Preprocessing(img)
-                if preprocess_config['algorithm'] == "sharp":
-                    preprocessed_img = preprocess.sharpen()
-                if preprocess_config['algorithm'] == "rescale":
-                    preprocessed_img = preprocess.rescale()
-                ######## END PREPROCESSING ########
-
-                ######## BEGIN THRESHOLDING ########
-                threshold_config = config['thresholding']
-                threshold = Thresholding(preprocessed_img, threshold_config)
-                thresholded_img = threshold.apply()
-                ######## END THRESHOLDING ########
-
-                ######## BEGIN POSTPROCESSING ########
-                postprocessing_config = config['postprocessing']
-                postprocess = Postprocessing(thresholded_img, postprocessing_config)
-                whole_img_not_cleaned, final_image, num_bacilli, stats = postprocess.apply()
-                total_number_bacilli += num_bacilli
-                ######## END POSTPROCESSING ########
-        print("Total number of bacilli: ", total_number_bacilli)  
-        print("Blurry images that were not considered: ", counter_of_blurry_images)
-
+        smear_pipeline(config, img)
     else:
-        ######## BEGIN PREPROCESSING ########
-        preprocess_config = config['preprocessing']
-        preprocess = Preprocessing(img)
-        if preprocess_config['algorithm'] == "sharp":
-            preprocessed_img = preprocess.sharpen()
-        if preprocess_config['algorithm'] == "rescale":
-            preprocessed_img = preprocess.rescale()
-        ######## END PREPROCESSING ########
-        
-        ######## BEGIN THRESHOLDING ########
-        threshold_config = config['thresholding']
-        threshold = Thresholding(preprocessed_img, threshold_config)
-        thresholded_img = threshold.apply()
-        ######## END THRESHOLDING ########
-        
-        ######## BEGIN POSTPROCESSING ########
-        postprocessing_config = config['postprocessing']
-        postprocess = Postprocessing(thresholded_img, postprocessing_config)
-        whole_img_not_cleaned, final_image, num_bacilli, stats = postprocess.apply()
-        ######## END POSTPROCESSING ########
-
-        ######## BEGIN ADD BOUNDING BOXES ########
-        image_boxes = add_bounding_boxes(img, stats)
-        ######## END BOUNDING BOXES ########
-
-        if postprocessing_config['crop']:
-            ######## BEGIN CROPPING ########
-            cropping_function = Cropping(img, final_image)
-            cropped_images = cropping_function.crop_and_pad()
-            ######## END CROPPING ########
-
-        labelling_dataset_config = config['labelling_dataset']
-        if labelling_dataset_config['create_dataset']:
-
-            ######## BEGIN INTERACTIVE LABELING #######
-            i_l = InteractiveLabeling(cropped_images)
-            labels = i_l.run()
-            ######### END INTERACTIVE LABELING ########
-
-            ######## BEGIN DATASET CREATION ########
-            dataframe = pd.DataFrame()
-            for i in range(0, labels.shape[0]):
-                d = {'image': [cropped_images[i]], 'label': [labels[i]]}
-                df2 = pd.DataFrame(d)
-                dataframe = pd.concat([dataframe, df2], ignore_index=True)
-            ######## END DATASET CREATION ########
-
-            ######## BEGIN SAVING ########
-            save_config = config['saving']
-            if save_config['save']:
-                # save dataframe with pandas library
-                labelled_data_path = os.path.join('labelled_data', loader.dataset_name + '.pkl')
-                dataframe.to_pickle(labelled_data_path)
-            ######## END SAVING ########
-
-        ######## BEGIN INFERENCE/VISUALIZATION ########
-        inference_config = config['inference']
-        if inference_config['do_inference']:
-            inference = Inference(cropped_images, stats)
-            red_boxes, green_boxes = inference.predict()
-
-            viewer = napari.Viewer()
-            viewer.add_image(img, name='Inferenced image')
-            viewer.add_shapes(red_boxes, shape_type='rectangle', edge_color='red',
-                              face_color='transparent', name='Not bacilli')
-            viewer.add_shapes(green_boxes, shape_type='rectangle', edge_color='green',
-                              face_color='transparent', name='Bacilli')
-            napari.run()
-        ######## END INFERENCE/VISUALIZATION ########
-            
-        ######## BEGIN VISUALIZATION ########
-        visualization_config = config['visualization']
-        show = visualization_config['show']
-        if show:
-            if preprocess_config['algorithm'] == 'rescale':
-                images = [img, whole_img_not_cleaned, final_image, image_boxes]
-                strings_names = ['original', 'binarized', 'cleaned binarized','original w/ boxes']
-            else:
-                images = [img, preprocessed_img, whole_img_not_cleaned, final_image, image_boxes]
-                algorithm = visualization_config['algorithm']
-                strings_names = ['original', f'{algorithm}ened', 'binarized', 'cleaned binarized','original w/ boxes']
-            visualize_all_list_napari(images, strings_names)
-        # currently opening the napari visualizer stops the execution of the code
-        ######## END VISUALIZATION ########
-        
+        tile_pipeline(config, img, loader)  
+              
 if __name__ == "__main__":
     main()
