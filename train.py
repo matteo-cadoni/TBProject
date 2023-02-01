5d11a90f49449dbaba8071b0c6a6aaa7053279a8
lorenzocapob <117085421+lorenzocapob@users.noreply.github.com>
2023-02-01 10:45:22 +0100

    changes to training pipeline

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from model.neural_net import Net, ChatGPT, MyDataset, toy_model
-from model.autoencoder import Autoencoder_conv, Autoencoder_mlp
+#from model.autoencoder import Autoencoder_conv, Autoencoder_mlp
 from data_augmentation import DataAug
 import pandas as pd
 from torch.utils.data import DataLoader
@@ -13,7 +13,13 @@ import argparse
 import yaml
 import time
 import os
-from torch.utils.tensorboard import SummaryWriter
+#from torch.utils.tensorboard import Summary#writer
+from matplotlib import pyplot as plt
+
+from train_utils.load import *
+from train_utils.filters import *
+from train_utils.evaluation import *
+from train_utils.train import *
 
 
 # randomly sampled subset of input dataframe with sampling probability p
@@ -33,42 +39,47 @@ def main():
     parser = arguments_parser()
     pars_arg = parser.parse_args()
     
-    
-    # set a seed for reproducibility
-    torch.manual_seed(42)
-    
     # read config as dictionary
     with open(pars_arg.config, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
+    # seed for reproducibility
+    random_state = config['random_state']
+
+    # set a seed for reproducibility
+    torch.manual_seed(random_state)
+    np.random.seed(random_state)
+
     # load data from a .pkl file
     loading_config = config['load']
     sampling_percentage = loading_config['sampling_percentage']
+
+    loadr = loader(loading_config)
+    data = loadr.load()
+    data = loadr.apply_filters(data)
+    time.sleep(2)
     print("--------------------------------------")
-    print('Loading data from pkl file')
-    data_paths = loading_config['data_path']
-    print('Data paths: ', data_paths)
-    data = []
-    for i, path in enumerate(data_paths):
-        data.append(pd.read_pickle(path))
-        print("Added data from path and shape: ", path, data[i].shape)
-    data = pd.concat(data)
-    print('Data loaded, shape: ', data.shape)
+    print("Applying filters to data")
+    filter_config = config['filter']
+    # if filter_config['remove_black_img']:
+    #     print("Removing black images")
+    #     data, n_removed_imgs = remove_black_img(data)
+    print("Removed ", n_removed_imgs, " black images")
+    print("Data shape is now: ", data.shape)
+    time.sleep(2)
     train_config = config['train']
     batch_size = train_config['batch_size']
     epochs = train_config['epochs']
-    test_eval = train_config['test_eval']
+
     
     print('Splitting data into train and test and preparing DataLoader')
+
     
-    # split data into train and test and set seed for reproducibility
-    np.random.seed(42)
-    
-    train, test = train_test_split(data, test_size=0.2, random_state=42)
+    train, test = train_test_split(data, test_size=0.2, random_state=random_state)
     print('Train and test data splitted, train shape: ', train.shape, 'test shape: ', test.shape)
     # sample a subset of train set
     print('Sampling train data, with sampling percentage: ', sampling_percentage * 100, '%')
-    train_sampled = sample(train, p = sampling_percentage, random_state=42)
+    train_sampled = sample(train, p = sampling_percentage, random_state=random_state)
     print('After sampling, new train dataset shape is ', train_sampled.shape)
     dataAug = DataAug(train_sampled)
     train = dataAug.augment()
@@ -81,149 +92,91 @@ def main():
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
     print("Test data is ready, lenght: ", len(test_dataset))
     print("--------------------------------------")
-    # train model
-    #net = Net()
+    time.sleep(2)
     net = ChatGPT()
-    #net = Autoencoder_conv()
     print("Model loaded")
     print(net)
     time.sleep(2)
 
     print('Initialising weights')
-    def weights_init(m):
-        if isinstance(m, nn.Conv2d):
-            torch.nn.init.xavier_uniform_(m.weight)
-            m.bias.data.fill_(0.01)
-        elif isinstance(m, nn.Linear):
-            torch.nn.init.xavier_uniform_(m.weight)
-            m.bias.data.fill_(0.01)
-        
     net.apply(weights_init)
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.backends.mps.is_available():
+        mps_device = torch.device("mps")
+        device = mps_device
     print("Device is: ", device)
     
     net = net.to(device)
     print("Model moved to device: ", device)
     
-    # writer for tensorboard
-    writer = SummaryWriter()
+    # #writer for tensorboard
+    #writer = Summary#writer()
+    
+    
     
     criterion = nn.BCELoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
+    
+    
     net.train()
+    tr_loss =[]
+    tst_loss = []
+    acc = []
     print("--------------------------------------")
     print("Training started")
     print("--------------------------------------")
     for ep in range(epochs):  # loop over the dataset multiple times
         train_loss = []
+        total_samples = []
         for i, data in enumerate(train_loader):
-            # get the inputs
-            inputs, labels = data
-
-            inputs=torch.unsqueeze(inputs,1)
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            # forward + backward + optimize
-            outputs = net(inputs)
-            outputs = outputs.squeeze(1)
-
-            loss = criterion(outputs, labels)
-            train_loss.append(loss.item())
-            
-            loss.backward()
-            optimizer.step()
-        total_train_loss = (np.sum(train_loss) / len(train_dataset)) * 10000
-        writer.add_scalar("Training Loss", total_train_loss, ep)
+            #perform training step
+            training_step(net, data, device, optimizer, criterion, train_loss, total_samples)
         
-        if ep % test_eval == 0:
-            net.eval()
-            test_loss = []
-            correct = 0
-            total = 0
-            with torch.no_grad():
-                for data in test_loader:
-                    images, labels = data
-                    images = images.to(torch.float32)
-                    images= images.view(images.shape[0],1,50,50)
-                    labels = labels.to(torch.float32)
-                    images = images.to(device)
-                    labels = labels.to(device)
-                
-                    outputs = net(images)
-                    outputs = outputs.squeeze(1)
-                    
-                    loss = criterion(outputs, labels)
-                    test_loss.append(loss.item())
-                    
-                    for i, output in enumerate(outputs):
-                        if output > 0.7:
-                            outputs[i] = 1
-                        else:
-                            outputs[i] = 0
-                    
-                    total += labels.size(0)    
-                    correct += (outputs == labels).sum().item()
-                accuracy = 100 * correct / total
-                total_test_loss = (np.sum(test_loss) / len(test_dataset)) * 10000
-                
-                writer.add_scalar("Test Accuracy", accuracy, ep)
-                writer.add_scalar("Test Loss", total_test_loss, ep)
+        #compute average train loss for this epoch
+        average_train_loss = np.mean(train_loss) * 1000
+        
+        total_test_loss, accuracy = evaluation_step(net, test_loader, device, criterion)
 
-            net.train()
-        print("Epoch: {}".format(ep+1))
-        print("Train Loss: {:f}".format(total_train_loss))
-        if ep % test_eval == 0:
-            print("Test Loss: {:f}, Accuracy {:f}".format(total_test_loss, accuracy))
+        print_statistics(ep, average_train_loss, total_test_loss, accuracy)        
+        tr_loss.append(average_train_loss)
+        tst_loss.append(total_test_loss)
+        acc.append
         print("------------------")
             
         
     print('Finished Training')
-    #print finished training statistics
+    
     print('Saving model')
     if not os.path.exists('model_ckpt'):
         os.makedirs('model_ckpt')
     torch.save(net.state_dict(), 'model_ckpt/model.pth')
     
-    writer.flush()
-    writer.close()
+    #writer.flush()
+    #writer.close()
 
-    # test model
-    #print('Testing model on validation set')
-    correct = 0
-    total = 0
-    net.eval()
-
-    with torch.no_grad():
-        for data in test_loader:
-            images, labels = data
-            images = images.to(torch.float32)
-            images= images.view(images.shape[0],1,50,50)
-            labels = labels.to(torch.float32)
-            images = images.to(device)
-            labels = labels.to(device)
-            
-            outputs = net(images)
-            #print("Shape of outputs and labels is: ", outputs.shape, labels.shape)
-            #print("outputs: ", outputs)
-            outputs = outputs.squeeze(1)
-            for i, output in enumerate(outputs):
-                if output > 0.5:
-                    outputs[i] = 1
-                else:
-                    outputs[i] = 0
-            #print("outputs and shape", outputs, outputs.shape)
-            #print("labels and shape", labels, labels.shape)
-            total += labels.size(0)
-            #print("total: ", total)
-            
-            correct += (outputs == labels).sum().item()
-            #print("correct: ", correct)
-            
-    print('Final accuracy of the network on the validation set: %d %%' % (
-            100 * correct / total))
+    evaluation_step(net, test_loader, device, criterion)
+    
+    print("Best accuracy is: ", max(acc))
+    # plot train and test loss over all epochs
+    
+    plt.plot(tr_loss)
+    plt.plot(tst_loss)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(['Train Loss', 'Test Loss'])
+    plt.title('Loss over all epochs')
+    plt.show()
+    
+    # plot test loss and accuracy over all epochs
+    plt.plot(acc)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy over all epochs')
+    plt.show()
+        
+        
+    
 
 
 if __name__ == '__main__':
