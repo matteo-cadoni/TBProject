2b51dcf0ff88efb153f690437470adf564984f39
Ubuntu Server <ubuntu@project-8.denbi.prod>
2023-02-01 10:06:54 +0000

    fixed problems

--- train_utils/filters.py
+++ train_utils/filters.py
@@ -24,3 +24,18 @@ class filter():
     def apply(self, data):
         if self.rmv_black_img:
             return self.remove_black_img(data)
+        
+        
+def remove_black_img(data):
+    # given an input dataframe, check each image whether maximum value of pixel is 0
+    n_removed_img = 0
+    filtered_data = pd.DataFrame()
+
+    for i, img in enumerate(tqdm.tqdm(data['image'], total=data.shape[0])):
+        if img.max() == 0:
+            n_removed_img += 1
+            continue
+        filtered_data = pd.concat([filtered_data, data.iloc[i]], axis=1)
+        
+        print("Removed {} black images".format(n_removed_img))
+    return filtered_data.transpose(), n_removed_img
